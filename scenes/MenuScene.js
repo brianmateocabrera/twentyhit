e5ac057a9dff5ab89e0ee13c04cda2ca683aad71
Brian Cabrera <57054546+brianmateocabrera@users.noreply.github.com>
2025-08-30 21:51:36 -0300

    Update MenuScene.js

--- scenes/MenuScene.js
+++ scenes/MenuScene.js
@@ -15,55 +15,51 @@ class MenuScene extends Phaser.Scene {
         const centerX = this.scale.width / 2;
         const centerY = this.scale.height / 2 - 100;
 
-        const logo = this.add.image(centerX, -200, "logo");
-        logo.setOrigin(0.5);
-        logo.setScale(1.5);
+        const logo = this.add.image(centerX, -200, "logo").setOrigin(0.5).setScale(1.5);
 
-        this.input.once("pointerdown", () => {
-            // reproducir música tras interacción
-            this.menuMusic = this.sound.add("menuMusic", { loop: true, volume: 0.5 });
-            this.menuMusic.play();
+        // reproducir música y sonido del presentador
+        this.menuMusic = this.sound.add("menuMusic", { loop: true, volume: 0.5 });
+        this.menuMusic.play();
+        this.sound.play("announcer", { volume: 1 });
 
-            this.sound.play("announcer", { volume: 1 });
+        // animación caída del logo
+        this.tweens.add({
+            targets: logo,
+            y: centerY,
+            duration: 326,
+            ease: "Bounce.easeOut",
+            onComplete: () => {
+                // balanceo del logo
+                this.tweens.add({
+                    targets: logo,
+                    angle: { from: -5, to: 5 },
+                    duration: 326,
+                    yoyo: true,
+                    repeat: -1,
+                    ease: "Sine.easeInOut"
+                });
 
-            // animación caída del logo
-            this.tweens.add({
-                targets: logo,
-                y: centerY,
-                duration: 326,
-                ease: "Bounce.easeOut",
-                onComplete: () => {
-                    // balanceo logo
-                    this.tweens.add({
-                        targets: logo,
-                        angle: { from: -5, to: 5 },
-                        duration: 326,
-                        yoyo: true,
-                        repeat: -1,
-                        ease: "Sine.easeInOut"
-                    });
+                // texto START
+                const jugarText = this.add.text(centerX, this.scale.height / 2 + 400, "START", {
+                    fontFamily: '"Press Start 2P"',
+                    fontSize: "32px",
+                    color: "#000"
+                }).setOrigin(0.5);
 
-                    const jugarText = this.add.text(centerX, this.scale.height / 2 + 400, "START", {
-                        fontFamily: '"Press Start 2P"',
-                        fontSize: "32px",
-                        color: "#000"
-                    });
-                    jugarText.setOrigin(0.5);
+                // parpadeo del texto START
+                this.tweens.add({
+                    targets: jugarText,
+                    alpha: { from: 1, to: 0 },
+                    duration: 600,
+                    yoyo: true,
+                    repeat: -1
+                });
 
-                    this.tweens.add({
-                        targets: jugarText,
-                        alpha: { from: 1, to: 0 },
-                        duration: 600,
-                        yoyo: true,
-                        repeat: -1
-                    });
-
-                    // iniciar GameScene
-                    this.input.once("pointerdown", () => {
-                        this.scene.start("GameScene");
-                    });
-                }
-            });
+                // click/tap para iniciar GameScene
+                this.input.once("pointerdown", () => {
+                    this.scene.start("GameScene");
+                });
+            }
         });
     }
 }
